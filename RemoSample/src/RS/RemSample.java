7a81636a71f0f0e07ab54e08d5b7a0e45da0288e
TayamaKoki <ko1228ki1228@gmail.com>
2020-03-27 02:42:37 +0900

    conflictの練習２

--- RemoSample/src/RS/RemSample.java
+++ RemoSample/src/RS/RemSample.java
@@ -4,7 +4,7 @@ public class RemSample {
 
 	public static void main(String[] args) {
 	// TODO 自動生成されたメソッド・スタブ
-		
+		System.out.println("practice for conflict.");
 		System.out.println("HelloWorld");
 		System.out.println("edit first by second pc.");
 		System.out.println("edit first by first pc.");
