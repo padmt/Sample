f8f03b0f75cd324d7034482c5416a1c9dc938205
TayamaKoki <ko1228ki1228@gmail.com>
2020-03-27 03:14:25 +0900

    fetsht

--- RemoSample/src/RS/RemSample.java
+++ RemoSample/src/RS/RemSample.java
@@ -5,7 +5,7 @@ public class RemSample {
 	public static void main(String[] args) {
 	// TODO 自動生成されたメソッド・スタブ
 		System.out.println("practice for conflict.");
-		
+		System.out.println("print");
 	}
 
 }
