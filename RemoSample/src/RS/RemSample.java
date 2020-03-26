be599101fda2a4f2d8e0165c931b66f04b189232
TayamaKoki <ko1228ki1228@gmail.com>
2020-03-27 03:12:57 +0900

    fyuk

--- RemoSample/src/RS/RemSample.java
+++ RemoSample/src/RS/RemSample.java
@@ -5,15 +5,7 @@ public class RemSample {
 	public static void main(String[] args) {
 	// TODO 自動生成されたメソッド・スタブ
 		System.out.println("practice for conflict.");
-		System.out.println("HelloWorld");
-		System.out.println("edit first by second pc.");
-		System.out.println("edit first by first pc.");
-		System.out.println("edit second by second pc.");
-		System.out.println("edit second by first pc.");
-		System.out.println("practice for conflict.");
-		System.out.println("practice for conflict.");
-		System.out.println("practice for conflict.");
-		System.out.println("practice for conflict.");
+		
 	}
 
 }
